5dd06010286287943f53c551ad30370ff3c028b1
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-01-13 13:04:17 -0500

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+
+const names=["Guadalupe", "Ollie" , "Aki"];
+function writeCards( names, event ) {
+    let completedCards = []
+    for(let i = 0; i < names.length; i++){
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(completedCards);
+    }
+    return completedCards;
+}
+
+writeCards(names, "Suprise")
+
+function countDown() {
+    let countDown = 10;
+while (countDown >= 0) {
+  console.log(countDown--);
+ }
+}
+countDown()
+
+
+
+
+
+
+
+
+
